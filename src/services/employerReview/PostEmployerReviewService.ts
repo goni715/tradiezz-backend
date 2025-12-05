8841bbbf1529383c4b9a63a7148becda4486940c
goni715 <halesgoni715149123@gmail.com>
2025-12-05 11:34:18 +0600

    Post employer review

--- src/services/employerReview/PostEmployerReviewService.ts
+++ src/services/employerReview/PostEmployerReviewService.ts
@@ -1,11 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import mongoose, { Types } from "mongoose";
 import CustomError from "../../errors/CustomError";
 import { IEmployerReview } from "../../interfaces/employerReview.interface";
 import ApplicationModel from "../../models/ApplicationModel";
 import EmployerReviewModel from "../../models/EmployerReviewModel";
+import CandidateModel from "../../models/CandidateModel";
 
 
 const PostEmployerReviewService = async (loginEmployerUserId: string, payload: IEmployerReview) => {
     const { jobId, candidateUserId } = payload;
+
+    //check candidate
+    const candidate = await CandidateModel.findOne({
+        userId: candidateUserId
+    });
+    if (!candidate) {
+        throw new CustomError(404, "Candidate Not Found with this provided ID");
+    }
 
     //check application
     const application = await ApplicationModel.findOne({
@@ -23,12 +34,12 @@ const PostEmployerReviewService = async (loginEmployerUserId: string, payload: I
         throw new CustomError(400,  "You can only review a candidate whose application has been accepted.")
     }
 
-    //check workStatus
+    // //check workStatus
     if(application.workStatus !== "completed"){
         throw new CustomError(400, "You can only submit a review after the job is completed.")
     }
 
-    //check already provided review
+    // //check already provided review
     const review = await EmployerReviewModel.findOne({
         jobId,
         employerUserId: loginEmployerUserId,
@@ -38,13 +49,58 @@ const PostEmployerReviewService = async (loginEmployerUserId: string, payload: I
         throw new CustomError(409, "You have already submitted a review for this candidate on this job.");
     }
 
-    //post review
-    const result = await EmployerReviewModel.create({
-        ...payload,
-        employerUserId: loginEmployerUserId
-    })
-  
-    return result;
+    //transacation & rollback
+    const session = await mongoose.startSession();
+
+    try {
+        session.startTransaction();
+
+        //post review
+        await EmployerReviewModel.create([{
+            ...payload,
+            employerUserId: loginEmployerUserId
+        }], { session })
+
+
+        //count average rating
+        const averageRatingResult = await EmployerReviewModel.aggregate([
+            {
+                $match: {
+                    candidateUserId: new Types.ObjectId(candidateUserId)
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    averageRating: {
+                        $avg: "$star"
+                    }
+                }
+            }
+        ],
+        { session })
+
+        const averageRating =
+            averageRatingResult.length > 0
+                ? Number((averageRatingResult[0].averageRating).toFixed(1))
+                : candidate.ratings;
+
+
+        //update candidate rating
+        await CandidateModel.updateOne(
+            { userId: candidateUserId },
+            { ratings: averageRating},
+            { session }
+        )
+
+        await session.commitTransaction();
+        await session.endSession();
+        return null;
+    } catch (err: any) {
+        await session.abortTransaction();
+        await session.endSession();
+        throw new Error(err);
+    }
 }
 
 export default PostEmployerReviewService
