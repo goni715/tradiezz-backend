ba5bc356fef0e2919ebfed8fa4b156f9dce88d81
goni715 <halesgoni715149123@gmail.com>
2025-12-08 17:46:06 +0600

    update singleCandidateService

--- src/services/user/GetCandidateService.ts
+++ src/services/user/GetCandidateService.ts
@@ -33,24 +33,6 @@ const GetCandidateService = async (userId: string) => {
             $unwind: "$subcategory"
         },
         {
-            $lookup: {
-                from: "reviews",
-                let: { uid: "$userId" },   //$$uid // <-- variable created here
-                pipeline: [
-                    { $match: { $expr: { $eq: ["$userId", "$$uid"] } } },
-                    { $count: "count" },
-                ],
-                as: "reviewCount"
-            }
-        },
-        {
-            $addFields: {
-                totalReview: {
-                    $ifNull: [{ $arrayElemAt: ["$reviewCount.count", 0] }, 0]
-                }
-            }
-        },
-        {
             $project: {
                 _id: 0,
                 userId: 1,
@@ -70,7 +52,7 @@ const GetCandidateService = async (userId: string) => {
                 address:"$address",
                 coordinates: "$location.coordinates",
                 ratings: "$ratings",
-                totalReview: "$totalReview",
+                totalReviews: "$totalReviews",
             }
         },
     ])
